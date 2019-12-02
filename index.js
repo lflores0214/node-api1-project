3d5384757e9d38f6d2a6d24025c78505db1ad613
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-02 16:34:40 -0700

    created delete request

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const db = require("./data/db.js");
 const server = express();
 
 server.use(express.json());
-
+// req === request res === response AKA "the homies"
 server.get("/", (req, res) => {
   res.send({ api: "up and running." });
 });
 
-//list of users
+//GET list of users
 server.get("/users", (req, res) => {
   db.find()
     .then(users => {
@@ -25,7 +25,7 @@ server.get("/users", (req, res) => {
     });
 });
 
-//create a user
+//POST create a user
 server.post("/users", (req, res) => {
   const userData = req.body;
   if (!userData.name || !userData.bio) {
@@ -43,10 +43,39 @@ server.post("/users", (req, res) => {
 });
 
 //GET user by id
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: " error retrieving user" });
+    });
+});
 
 //DELETE user by id
+server.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
 
+  db.remove(id)
+    .then(removed => {
+      if (removed) {
+        res.status(200).json({ message: "user deleted succesfully" });
+      } else {
+        res.status(500).json({ Message: "user not found" });
+      }
+    })
+    .catch(error => {
+      console.log("error on delete /users", error);
+      res.status(500).json({ errorMessage: "error deleting user" });
+    });
+});
 //PUT updates the user info with data from request body. returns modified document
+
+server.put("users/:id", (req, res) => {
+
+})
 
 const port = 5000;
 server.listen(port, () =>
